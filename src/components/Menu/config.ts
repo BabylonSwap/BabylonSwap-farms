df3dcb3537264f564d41a0847f5719de7ff64e94
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-06-29 21:56:12 +1000

    update

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,7 +4,7 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://https://babylonswap.finance/',
+    href: 'https://babylonswap.finance/#/',
   },
   {
     label: 'Trade',
